a1316435fcdfa3242dc679a22d7065afc791198c
dimashki98 <najdshaqra@gmail.com>
2024-11-08 21:09:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     // يمكنك إضافة حركات أو وظائف هنا لاحقًا
 });
+
+function openModal(image, link) {
+    const modal = document.getElementById("imageModal");
+    const modalImage = document.getElementById("modalImage");
+    const modalLink = document.getElementById("modalLink");
+
+    modal.style.display = "flex";
+    modalImage.src = image.src;
+    modalLink.href = link;
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
